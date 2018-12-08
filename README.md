da10039648e741176c3af632740a372eaa630589
Maxime Rauer <maxime.rauer@gmail.com>
2018-12-08 12:17:14 -0600

    issues/mrauer/initial-functions/2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Divyield
+# DivYield
 
-**Divyield** is a sofware designed to catch high payout dividends with low variability and long history.
+**DivYield** is a sofware designed to catch high payout dividends with low variability and long history.
 
 Current version **0.1**
 
@@ -14,5 +14,10 @@ We will be identifying the following attributes at the stock level:
 
 ## Endpoints
 * https://api.iextrading.com/1.0/ref-data/symbols (Symbols)
-* https://api.iextrading.com/1.0/stock/mmm/chart/5y
+* https://api.iextrading.com/1.0/stock/mmm/chart/1m
 * https://api.iextrading.com/1.0/stock/mmm/dividends/5y
+
+## Commands
+    python -m virtualenv env
+    ./env/bin/python main.py '{"action": "update"}'
+    ./env/bin/python main.py '{"action": "dividends"}'
--- divyield.py
+++ divyield.py
@@ -1,1 +1,43 @@
-TBD
+import statistics
+
+import requests
+
+
+def get_symbols():
+    """Get symbols and overwrite input file."""
+    r = requests.get('https://api.iextrading.com/1.0/ref-data/symbols')
+    symbols = r.json()
+    f = open('./symbols.dat', 'w')
+    for symbol in symbols:
+        if symbol['isEnabled']:
+            f.write(''.join([symbol['symbol'], '\n']))
+    f.close()
+    return len(symbols)
+
+
+def get_dividends(stock_symbol):
+    """Get 5yr dividends when available."""
+    api_url = '/'.join(['https://api.iextrading.com/1.0/stock',
+                        stock_symbol, 'dividends/5y'])
+    r = requests.get(api_url)
+    response = r.json()
+    history = len(response)
+    records = []
+    for record in response:
+        records.append(record['amount'])
+    avg = statistics.mean(records)
+    stdev = statistics.stdev(records)
+    return history, avg, stdev
+
+
+def get_chart(stock_symbol):
+    """Get stock info such as current valuation."""
+    api_url = '/'.join(['https://api.iextrading.com/1.0/stock',
+                        stock_symbol, 'chart/1m'])
+    r = requests.get(api_url)
+    response = r.json()
+    records = []
+    for record in response:
+        records.append(record['close'])
+    stock_price = records[-1]
+    return stock_price
